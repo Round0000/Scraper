5069aa81082d2b3dc3a7b55e926e59549232e11c
Round0000 <billybinche@gmail.com>
2021-12-03 10:15:11 +0100

    commit

--- public/script.js
+++ public/script.js
@@ -18,7 +18,6 @@ document
     document.getElementById("result").textContent = "Please wait...";
 
     fetch("/.netlify/functions/get-boarding-gate", options)
-      .then((res) => res.json())
       .then((res) => {
         document.getElementById("result").textContent = res;
       })
