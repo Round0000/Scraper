5c304af72f456eb26029b58172042bb0ac6bf36c
Round0000 <billybinche@gmail.com>
2021-12-03 09:31:33 +0100

    commit

--- functions/get-boarding-gate.js
+++ functions/get-boarding-gate.js
@@ -1,36 +1,37 @@
-const chromium = require('chrome-aws-lambda');
+const chromium = require("chrome-aws-lambda");
 
 exports.handler = async (event, context) => {
-
   const pageToScrap = JSON.parse(event.body).pageToScrap;
 
-  if (!pageToScrap) return {
+  if (!pageToScrap)
+    return {
       statusCode: 400,
-      body: JSON.stringify({ message: 'Page URL not defined' })
-  }
+      body: JSON.stringify({ message: "Page URL not defined" }),
+    };
 
   const browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
   });
 
   const page = await browser.newPage();
 
-  await page.goto(pageToScrap, { waitUntil: 'networkidle2' });
+  await page.goto(pageToScrap, { waitUntil: "networkidle2" });
 
   const info = await page.evaluate(() => {
     let gate = document.querySelector("h1").textContent;
     console.log("wesh " + gate);
-    return gate
-  })
+    return gate;
+  });
 
   await browser.close();
 
   return {
-      statusCode: 200,
-      body: info
-  }
-
-}
+    statusCode: 200,
+    body: JSON.stringify({
+      info,
+    }),
+  };
+};
