c601a9d56ea6da11135980e46d6418d858813848
Round0000 <billybinche@gmail.com>
2021-12-03 09:17:31 +0100

    commit

---
+++ functions/get-boarding-gate.js
@@ -0,0 +1,42 @@
+https://www.flightview.com/flight-tracker/AF/1600?date=20211203&depapt=CDG
+
+
+const chromium = require('chrome-aws-lambda');
+
+exports.handler = async (event, context) => {
+
+  const pageToScrap = JSON.parse(event.body).pageToScrap;
+
+  if (!pageToScrap) return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Page URL not defined' })
+  }
+
+  const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(pageToScrap, { waitUntil: 'networkidle2' });
+
+  const info = await page.evaluate(() => {
+    let gate = document.querySelector("#ffDepartureInfo > tbody > tr:nth-child(4) > td");
+
+    return gate
+  })
+
+  await browser.close();
+
+  return {
+      statusCode: 200,
+      body: JSON.stringify({
+          message: `Boarding gate :`,
+          info
+      })
+  }
+
+}
--- public/script.js
+++ public/script.js
@@ -1,42 +1,32 @@
-function bufferToImageUrl(buffer) {
-  // See https://gist.github.com/candycode/f18ae1767b2b0aba568e
 
-  var arrayBufferView = new Uint8Array( buffer );
-  var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
-  var urlCreator = window.URL || window.webkitURL;
-  var imageUrl = urlCreator.createObjectURL( blob );
+document
+  .querySelector('button[type="submit"]')
+  .addEventListener("click", (e) => {
+    e.preventDefault();
 
-  return imageUrl;
-}
+    const pageToScrap = document.getElementById("page").value;
 
-document.querySelector('button[type="submit"]').addEventListener('click', (e) => {
+    if (!pageToScrap)
+      return (document.getElementById("result").textContent =
+        "Please enter a page URL");
 
-  e.preventDefault();
-
-  const pageToScreenshot = document.getElementById('page').value;
-
-  if (!pageToScreenshot) return document.getElementById('result').textContent = 'Please enter a page URL';
-
-  const options = {
+    const options = {
       method: "POST",
       headers: { "Content-Type": "application/json; charset=utf-8" },
-      body: JSON.stringify({ pageToScreenshot: pageToScreenshot })
-  };
+      body: JSON.stringify({ pageToScrap: pageToScrap }),
+    };
 
-  document.getElementById('result').textContent = "Please wait..."
+    document.getElementById("result").textContent = "Please wait...";
 
-  fetch("/.netlify/functions/take-screenshot", options)
+    fetch("/.netlify/functions/get-boarding-gate", options)
       .then((res) => res.json())
       .then((res) => {
-
-          if (!res.buffer) return document.getElementById('result').textContent = 'Error capturing screenshot';
-
-          const img = document.createElement('img');
-          img.src = bufferToImageUrl(res.buffer.data);
-          document.getElementById('result').innerHTML = img.outerHTML;
+        console.log(res);
       })
       .catch((err) => {
-          console.log(err)
-          document.getElementById('result').textContent = `Error: ${err.toString()}`
+        console.log(err);
+        document.getElementById(
+          "result"
+        ).textContent = `Error: ${err.toString()}`;
       });
-});
+  });
