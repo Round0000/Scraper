631482000b9f2fd5ec0cfc319d48e0f46cea6a9c
Round0000 <billybinche@gmail.com>
2021-12-03 16:44:41 +0100

    local working

--- local.js
+++ local.js
@@ -5,13 +5,7 @@ async function scrape(url) {
   const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: "networkidle2" });
-  const href = await page.evaluate(() => {
-    let link = document.querySelector(
-      "div.table__Table-sc-1x7nv9w-6.zGTLC > a:nth-child(3)"
-    ).href;
-    return link;
-  });
-  await page.goto(href, { waitUntil: "networkidle2" });
+
   // const info = await page.evaluate(() => {
   //   let result = document.querySelectorAll("div[class^='table__TableRow']");
   //   let data = [];
@@ -22,32 +16,82 @@ async function scrape(url) {
   // });
 
   const response = await page.evaluate(() => {
-    const number = document.querySelector(
-      "#__next > div > section > div:nth-child(3) > div > div:nth-child(2) > div > div > div > div > div > div > div > div"
-    ).innerText;
+    let flights = [];
 
-    const destination = document.querySelectorAll(
-      "a[class^='route-with-plane']"
-    )[1].innerText;
+    const results = document.querySelectorAll(".flight-col__airline");
 
-    const gate = document.querySelectorAll("div[class^='ticket__TGBValue']")[1]
-      .innerText;
+    results.forEach((item) => {
+      if (
+        item.innerText.includes("Air France") ||
+        item.innerText.includes("KLM")
+      ) {
+        let obj = {};
 
-    const flightInfo = {
-      number: number,
-      destination: destination,
-      gate: gate,
-    };
+        let result = item.parentElement.parentElement;
 
-    return flightInfo;
+        obj.flightNumber = result.querySelector(
+          ".flight-col__flight a"
+        ).innerText;
+        obj.scheduledTime = result.querySelector(".flight-col__hour").innerText;
+        obj.destination = result.querySelector(
+          ".flight-col__dest-term"
+        ).innerText;
+        obj.link = result.querySelector(".flight-col__flight--link").href;
+
+        flights.push(obj);
+      }
+    });
+
+    return flights;
   });
+
+  //
 
   await browser.close();
 
-  console.log(response);
+  console.log("FIRST RESPONSE FROM SCRAP FUNCTION :", response[0]);
+  return response;
 }
 
-scrape(
-  "https://www.flightstats.com/v2/flight-tracker/departures/CDG/AF?year=2021&month=12&date=3"
-);
+async function getGates(data) {
+  data = [data[0], data[1], data[2]];
+  let finalData = [];
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
+
+  console.log("data length in getGates function", data.length);
+
+  for (let i = 0; i < data.length; i++) {
+    await page.goto(data[i].link, { waitUntil: "networkidle2" });
+
+    const gate = await page.evaluate(() => {
+      let gateFound = document.querySelector(
+        ".flight-info__infobox div + div + div div:last-child"
+      ).innerText;
+
+      return gateFound;
+    });
+
+    data[i].gate = gate;
+    finalData.push(data[i]);
+  }
+
+  await page.close();
+
+  await browser.close();
+  console.log("FINAL DATA :", finalData);
+  return finalData;
+}
+
+const data = scrape(
+  "https://www.airport-charles-de-gaulle.com/cdg-departures-terminal-2F?tp=6"
+).then((data) => {
+  return getGates(data).then((res) => {
+    console.log("oulala", res);
+  });
+});
+
+// console.log(data.link);
+
+// getGate(data.link);
