a6f86cfe33b1904b56d1467a7e31bdbdf3e04961
Round0000 <billybinche@gmail.com>
2021-12-03 18:49:22 +0100

    commit

--- functions/get-boarding-gate.js
+++ functions/get-boarding-gate.js
@@ -9,101 +9,52 @@ exports.handler = async (event, context) => {
       body: JSON.stringify({ message: "Page URL not defined" }),
     };
 
-
-
-  async function scrape(url) {
-    const browser = await chromium.puppeteer.launch({
+  const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
     headless: chromium.headless,
   });
-    const page = await browser.newPage();
 
-    await page.goto(url, { waitUntil: "networkidle2" });
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    const response = await page.evaluate(() => {
-      let flights = [];
+  await page.goto(url, { waitUntil: "networkidle2" });
 
-      const results = document.querySelectorAll(".flight-col__airline");
+  const response = await page.evaluate(() => {
+    let flights = [];
 
-      results.forEach((item) => {
-        if (
-          item.innerText.includes("Air France") ||
-          item.innerText.includes("KLM")
-        ) {
-          let obj = {};
+    const results = document.querySelectorAll(".flight-col__airline");
 
-          let result = item.parentElement.parentElement;
+    results.forEach((item) => {
+      if (
+        item.innerText.includes("Air France") ||
+        item.innerText.includes("KLM")
+      ) {
+        let obj = {};
 
-          obj.flightNumber = result.querySelector(
-            ".flight-col__flight a"
-          ).innerText;
-          obj.scheduledTime = result.querySelector(".flight-col__hour").innerText;
-          obj.destination = result.querySelector(
-            ".flight-col__dest-term"
-          ).innerText;
-          obj.link = result.querySelector(".flight-col__flight--link").href;
+        let result = item.parentElement.parentElement;
 
-          flights.push(obj);
-        }
-      });
-
-      return flights;
-    });
-
-    //
-
-    await browser.close();
-
-    console.log("FIRST RESPONSE FROM SCRAP FUNCTION :", response[0]);
-    return response;
-  }
-
-  async function getGates(data) {
-    data = [data[0], data[1], data[2]];
-    let finalData = [];
-    const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-  });
-    const page = await browser.newPage();
-
-
-    console.log("data length in getGates function", data.length);
-
-    for (let i = 0; i < data.length; i++) {
-      await page.goto(data[i].link, { waitUntil: "networkidle2" });
-
-      const gate = await page.evaluate(() => {
-        let gateFound = document.querySelector(
-          ".flight-info__infobox div + div + div div:last-child"
+        obj.flightNumber = result.querySelector(
+          ".flight-col__flight a"
         ).innerText;
+        obj.scheduledTime = result.querySelector(".flight-col__hour").innerText;
+        obj.destination = result.querySelector(
+          ".flight-col__dest-term"
+        ).innerText;
+        obj.link = result.querySelector(".flight-col__flight--link").href;
 
-        return gateFound;
-      });
-
-      data[i].gate = gate;
-      finalData.push(data[i]);
-    }
-
-    await page.close();
-
-    await browser.close();
-    console.log("FINAL DATA :", finalData);
-    return finalData;
-  }
-
-  const data = scrape(
-    "https://www.airport-charles-de-gaulle.com/cdg-departures-terminal-2F?tp=6"
-  ).then((data) => {
-    return getGates(data).then((res) => {
-      console.log("oulala", res);
-      return res;
+        flights.push(obj);
+      }
     });
+
+    return flights;
   });
 
-  return data
+  //
+
+  await browser.close();
+
+  console.log(response);
+  return response
 };
